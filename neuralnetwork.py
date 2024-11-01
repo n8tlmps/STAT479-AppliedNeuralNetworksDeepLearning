d85565637348e9f0b6eafa495f19b9163431d9ec
Nathaniel Talampas <110523334+n8tlmps@users.noreply.github.com>
2024-10-31 21:55:47 -0700

    Create neuralnetwork.py

---
+++ neuralnetwork.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class NeuralNetwork:
+  def __init__(self, input_size, hidden_size, output_size):
+    self.input_size = input_size
+    self.hidden_size = hidden_size
+    self.output_size = output_size
+
+  # initialize weights
